bf98d66745987abc5f19f050b5e9de02020d389c
mocha <3129210+Mocha2007@users.noreply.github.com>
2021-11-16 10:20:36 -0500

    Create info.js

---
+++ commands/info.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const infoString =
+`mobot-js
+a rewrite of Mobot by mocha2007
+invite link:
+https://discord.com/oauth2/authorize?client_id=909942496329621535&scope=bot&permissions=2048&redirect_uri=http%3A%2F%2Flocalhost%3A53134&scope=applications.commands`;
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('info')
+		.setDescription('bot info'),
+	async execute(interaction) {
+		return interaction.reply(infoString);
+	},
+};
